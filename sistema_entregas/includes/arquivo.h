1a6a8b6ac84144504d4a4b21708845111c4c12c3
Bernardo Demaria Santos <bernardodemaria7@gmail.com>
2025-06-30 09:43:13 -0300

    feito arquivo .h e correçoes de caminho

--- sistema_entregas/includes/arquivo.h
+++ sistema_entregas/includes/arquivo.h
@@ -1,9 +1,9 @@
 #ifndef ARQUIVO_H
 #define ARQUIVO_H
 
-#include "local.h"
-#include "veiculo.h"
-#include "pedido.h"
+#include "../includes/local.h"
+#include "../includes/veiculo.h"
+#include "../includes/pedido.h"
 
 void backup(Local locais[], int numLocais, Veiculo veiculos[], int numVeiculos, Pedido pedidos[], int numPedidos);
 void restore(Local locais[], int *numLocais, Veiculo veiculos[], int *numVeiculos, Pedido pedidos[], int *numPedidos);
