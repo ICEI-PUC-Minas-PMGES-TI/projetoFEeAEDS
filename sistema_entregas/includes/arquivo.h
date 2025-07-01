c807a0fe7a2bbc747994b1786d50ba83afe67a3b
Thierry07 <raphaelthierry08@gmail.com>
2025-07-01 16:58:52 -0300

    Correcao de bugs no arquivo.h

--- sistema_entregas/includes/arquivo.h
+++ sistema_entregas/includes/arquivo.h
@@ -7,10 +7,10 @@
 
 void salvarLocais(Local locais[], int numLocais);
 void salvarVeiculos(Veiculo veiculos[], int numVeiculos);
-void salvarPedidos(Pedido pedidos[], int numPedidos);
+void salvarPedidos(Pedido pedidos[], int numPedidos, const char* nomeArquivo);
 
 void carregarLocais(Local locais[], int *numLocais);
 void carregarVeiculos(Veiculo veiculos[], int *numVeiculos);
-void carregarPedidos(Pedido pedidos[], int *numPedidos);
+void carregarPedidos(Pedido pedidos[], int *numPedidos, const char* nomeArquivo);
 
 #endif
